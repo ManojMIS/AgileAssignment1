349cb269c0d9f658d1c2dfde640a67690d0886ef
Premnath0210 <premanth.e2023@vitstudent.ac.in>
2026-01-08 20:52:02 +0530

    Modified test.py in branch-B

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ print("HELLO WORLD")
 print("Agile Lab")
 def login(user,password):
     print("Login Successful")
+# This line was added in branch-B - will cause conflict
+print("Modified by Premnath in branch-B")
+    
